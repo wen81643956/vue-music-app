55fee19b3dd19b02f29d16e0057806d2ca757825
bowendeng <dengbw@sctek.com>
2017-07-18 19:03:05 +0800

    增加轮播图

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot>
       </slot>
     </div>
     <div class="dots">
+      <span class="dot" v-for="(item, index) in dots" :class="{'active': currentPageIndex === index}"></span>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import { addClass } from 'common/js/dom'
   export default {
+    data () {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
     props: {
       loop: {
         type: Boolean,
@@ -22,8 +31,81 @@
       },
       interval: {
         type: Number,
-        default: 400
+        default: 4000
       }
+    },
+    mounted () {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initDots()
+        this._initSilder()
+        if (this.autoPlay) {
+          this._play()
+        }
+      }, 20)
+      window.addEventListener('resize', this._resizeWindow)
+    },
+    methods: {
+      _resizeWindow () {
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+      },
+      _setSliderWidth (isResize) {
+        this.children = this.$refs.sliderGroup.children
+        let width = 0
+        let silderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+          child.style.width = silderWidth + 'px'
+          width += silderWidth
+        }
+
+        if (this.loop && !isResize) {
+          width += 2 * silderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSilder () {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400
+        })
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          if (this.loop) {
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex
+
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      _initDots () {
+        this.dots = new Array(this.children.length)
+      },
+      _play () {
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) {
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400)
+        }, this.interval)
+      }
+    },
+    beforeDestory () {
+      window.removeEventListener('resize', this._resizeWindow)
     }
   }
 </script>
