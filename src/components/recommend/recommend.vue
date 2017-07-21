5419ab0904b0a1ba396b52e4042ae9c732e6fae1
bowendeng <dengbw@sctek.com>
2017-07-21 17:28:42 +0800

    歌手列表页面

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="recommend">
-    <scroll class="recommend-content" :data="discList">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
       <div>
         <div v-if="recommends.length" class="slider-wrapper">
           <slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="68" height="68" :src="item.imgurl">
+                <img width="68" height="68" v-lazy="item.imgurl">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,6 +26,9 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -33,6 +36,7 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
+  import Loading from 'base/loading/loading'
   import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
 
@@ -45,13 +49,12 @@
     },
     created () {
       this._getRecommond()
-      setTimeout(() => {
-        this._getDiscList()
-      }, 20)
+      this._getDiscList()
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     },
     methods: {
       _getRecommond () {
@@ -67,6 +70,12 @@
             this.discList = res.data.list
           }
         })
+      },
+      loadImage () {
+        if (!this.checkLoaded) {
+          this.checkLoaded = true
+          this.$refs.scroll.refresh()
+        }
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 fastclick.attach(document.body)
 
